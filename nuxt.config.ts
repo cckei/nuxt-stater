06622ef230d913b8db340c726a239d672b3696e3
kei <kei.chan@tbwa.com>
2024-08-29 17:26:00 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss'
+  ],
+  css: ['@/styles/main.scss'],
+  ssr: true,
+  i18n:{
+    locales: [{
+      code: 'en',
+      language: 'en-US',
+      file: 'en.json'
+    }, {
+      code: 'tc',
+      language: 'zh-HK',
+      file: 'tc.json'
+    }],
+    lazy: true,
+    langDir: 'lang',
+    defaultLocale: 'en',
+    strategy: 'prefix_and_default'
+  },
+  generate: {
+    routes: [
+      '/en/',
+      '/tc/'
+    ],
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { locale, setLocale } = useI18n()
+</script>
+
+<template>
+  <div>
+    <div>
+      <button @click="setLocale('en')">en</button>
+      <button @click="setLocale('tc')">tc</button>
+      <p>{{ $t('hello') }}</p>
+    </div>
+  </div>
+</template>
